c75d69d2e7d5fe8919b0dd72ef415565ac257314
JuanPablo Ramos <jprscs@gmail.com>
2020-02-04 13:13:49 -0600

    implement set for pids in cpu

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -10,13 +10,17 @@ import (
 type CPU struct {
 	realMemory mem.Memory
 	swapMemory mem.Memory
+	pids       set
 }
+
+type set map[string]bool
 
 // New creates a new CPU
 func New(real, swap mem.Memory) CPU {
 	return CPU{
 		realMemory: real,
 		swapMemory: swap,
+		pids:       make(map[string]bool),
 	}
 }
 
@@ -26,18 +30,28 @@ func (c *CPU) CreateProcess(pid string, size int) error {
 		return fmt.Errorf("size should not be 0")
 	}
 
+	present := c.pids[pid]
+	if present {
+		return fmt.Errorf("PID %s is already in cpu", pid)
+	}
+
 	requiredPages := size / c.realMemory.PageSize
 	for i := 0; i < requiredPages; i++ {
 		if ok := c.realMemory.AllocatePage(pid, i); !ok {
 			return fmt.Errorf("not enough space in memory for PID: %s", pid)
 		}
 	}
+	c.pids[pid] = true
 	// TODO: Check swap
 	return nil
 }
 
 // AccessProcess ...
 func (c *CPU) AccessProcess(pid string, addr int) (int, error) {
+	if present := c.pids[pid]; !present {
+		return -1, fmt.Errorf("PID %s is not present in cpu", pid)
+	}
+
 	rAddr, ok := c.realMemory.AccessPage(pid, addr)
 	if !ok {
 		return -1, fmt.Errorf("address %d for PID %s not found", addr, pid)
@@ -55,5 +69,6 @@ func (c *CPU) DeleteProcess(pid string) error {
 	if !foundInRealMem {
 		return fmt.Errorf("PID %s not found in memory", pid)
 	}
+	c.pids[pid] = false
 	return nil
 }
